8954fb413e160d69c2298d8000a342020cc8292a
Teddi Maull <teddi@picturetheday.com>
2015-04-18 11:47:35 -0600

    Make code a function that can be called

--- ch2/3_chessboard.js
+++ ch2/3_chessboard.js
@@ -1,28 +1,36 @@
-var char1 = ' ';
-var char2 = '#';
-var newline = '\n';
-var rows = '';
-var size = 5;
-var even_grid = (size%2 === 0)
-var toggle_char = true;
 
-for (var width = 1; width < size*size + 1; width++) {
-  end_of_line = (width%size === 0)
-  if (toggle_char) {
-    rows += char1;
-  }
-  else {
-    rows += char2;
-  }
-  if (end_of_line && even_grid) {
-    rows += newline;
+
+function make_grid(char1, char2, size) {
+  var even_grid = (size%2 === 0)
+  var toggle_char = true;
+  var newline = '\n';
+  var rows = '';
+
+  for (var width = 1; width < size*size + 1; width++) {
+    end_of_line = (width%size === 0)
+    if (toggle_char) {
+      rows += char1;
+    }
+    else {
+      rows += char2;
+    }
+    if (end_of_line && even_grid) {
+      rows += newline;
+      toggle_char = !toggle_char;
+    }
+    else if (end_of_line && !even_grid) {
+      rows += newline;
+    }
+
     toggle_char = !toggle_char;
   }
-  else if (end_of_line && !even_grid) {
-    rows += newline;
-  }
-
-  toggle_char = !toggle_char;
+  return rows;
 }
 
-  console.log(rows);
+
+  console.log("1x1 grid:\n" + make_grid("_", "#", 1));
+  console.log("2x2 grid:\n" + make_grid("_", "#", 2));
+  console.log("3x3 grid:\n" + make_grid("_", "#", 3));
+  console.log("4x4 grid:\n" + make_grid("_", "#", 4));
+  console.log("5x5 grid:\n" + make_grid("_", "#", 5));
+  console.log("8x8 grid:\n" + make_grid("_", "#", 8));
