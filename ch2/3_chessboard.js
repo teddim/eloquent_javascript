2ab99d88ce0f461eea266b6b96e5fe545fb17844
Teddi Maull <teddi@picturetheday.com>
2015-04-18 15:12:34 -0600

    Refactor code so that it is clearer

--- ch2/3_chessboard.js
+++ ch2/3_chessboard.js
@@ -1,36 +1,41 @@
 
-
-function make_grid(char1, char2, size) {
-  var even_grid = (size%2 === 0)
-  var toggle_char = true;
+function make_grid(size) {
   var newline = '\n';
   var rows = '';
 
+  var obj = new Object({
+    start_char: ' ',
+    fill_char: '#',
+
+    swap: function() {
+      var tmp = this.start_char;
+      this.start_char = this.fill_char;
+      this.fill_char = tmp;
+    }
+  });
+
   for (var width = 1; width < size*size + 1; width++) {
-    end_of_line = (width%size === 0)
-    if (toggle_char) {
-      rows += char1;
+
+    if (width%2 === 0) {
+    rows += obj.start_char;
     }
     else {
-      rows += char2;
-    }
-    if (end_of_line && even_grid) {
-      rows += newline;
-      toggle_char = !toggle_char;
-    }
-    else if (end_of_line && !even_grid) {
-      rows += newline;
+    rows += obj.fill_char;
     }
 
-    toggle_char = !toggle_char;
+    if (width%size === 0) {
+      rows += newline;
+      if (size%2 === 0) {
+        obj.swap();
+      }
+    }
   }
   return rows;
 }
 
-
-  console.log("1x1 grid:\n" + make_grid("_", "#", 1));
-  console.log("2x2 grid:\n" + make_grid("_", "#", 2));
-  console.log("3x3 grid:\n" + make_grid("_", "#", 3));
-  console.log("4x4 grid:\n" + make_grid("_", "#", 4));
-  console.log("5x5 grid:\n" + make_grid("_", "#", 5));
-  console.log("8x8 grid:\n" + make_grid("_", "#", 8));
+  console.log("1x1 grid:\n" + make_grid(1));
+  console.log("2x2 grid:\n" + make_grid(2));
+  console.log("3x3 grid:\n" + make_grid(3));
+  console.log("4x4 grid:\n" + make_grid(4));
+  console.log("5x5 grid:\n" + make_grid(5));
+  console.log("8x8 grid:\n" + make_grid(8));
