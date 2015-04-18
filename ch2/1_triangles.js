bdf35c6f0880d85223935c8f5e96ccbd670693f1
Teddi Maull <teddi@picturetheday.com>
2015-04-18 15:11:11 -0600

    Solve with arrays or strings

--- ch2/1_triangles.js
+++ ch2/1_triangles.js
@@ -1,6 +1,15 @@
-  var array = [];
-  var oc = '#';
-  for (var i = 0; i < 8; i++) {
-    array.push(oc);
-    console.log(array.join(''));
-  }
+//solve with array
+var array = [];
+var oc = '#';
+for (var i = 0; i < 8; i++) {
+  array.push(oc);
+  console.log(array.join(''));
+}
+
+//solve with a string
+var triangle = '';
+var oc = '#';
+for (var i = 0; i < 8; i++) {
+  triangle += oc;
+  console.log(triangle);
+}
