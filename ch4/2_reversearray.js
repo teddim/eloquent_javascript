f4438d630ba21427ed24be5b5850c3adaaa70b09
Teddi Maull <teddi@picturetheday.com>
2015-04-19 13:26:59 -0600

    Answer questions about the two versions of the function

--- ch4/2_reversearray.js
+++ ch4/2_reversearray.js
@@ -1,3 +1,10 @@
+// Thinking back to the notes about side effects and pure functions in the previous chapter, which variant do you expect to be useful in more situations? Which one is more efficient?
+
+//  I would expect reverseArray to be useful in more situations because it has a return value that other functions can do things with.
+
+// I would expect the reverseArrayInPlace variant to be more efficient because I think it uses less memory than reverseArray does. The array is stored once plus half of it is temporarily stored throughout the function in reverseArrayInPlace vs having two full copies of the array in the reverseArray function.
+
+
 function reverseArray(input_array) {
   var new_array = [];
   for (var i = input_array.length-1 ; i >= 0; i--) {
@@ -5,7 +12,6 @@ function reverseArray(input_array) {
   }
   return new_array;
 }
-
 
 function reverseArrayInPlace(arrayValue) {
   function swap(a,b) {
